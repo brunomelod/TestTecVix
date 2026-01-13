7977def773b0cb531d925c2c71cf43231835a6c5
Bruno Melo <brunomelod@gmail.com>
2026-01-13 15:12:46 -0300

    update PrivatePage.tsx

--- frontend-react-vix-test/src/auth/PrivatePage.tsx
+++ frontend-react-vix-test/src/auth/PrivatePage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useZResetAllStates } from "../stores/useZResetAllStates";
 import { FullPage } from "../components/Skeletons/FullPage";
 import { useEffect, useState } from "react";
+import { useZUserProfile } from "../stores/useZUserProfile";
 
 interface IProps {
   children: React.ReactNode;
@@ -19,27 +20,28 @@ export const PrivatePage = ({
   const [isChecking, setIsChecking] = useState(true);
   const { resetAllStates } = useZResetAllStates();
   const navigate = useNavigate();
-
+  const { idUser, token, role } = useZUserProfile();
   useEffect(() => {
+   
     switch (true) {
-      // case !idUser:
-      //   resetAllStates();
-      //   navigate("/login");
-      //   break;
-      // case onlyAdmin && role !== "admin":
-      //   navigate(-1);
-      //   break;
-      // case onlyManagerOrAdmin && role !== "admin" && role !== "manager":
-      //   navigate(-1);
-      //   break;
+      case !idUser || !token: // Se não tem usuário ou token logado
+        resetAllStates();
+        navigate("/login");
+        break;
+      case onlyAdmin && role !== "admin": // Se precisa ser admin mas não é
+        navigate(-1);
+        break;
+      case onlyManagerOrAdmin && role !== "admin" && role !== "manager": // Se precisa ser manager/admin mas não é
+        navigate(-1);
+        break;
 
       default:
         setIsChecking(false);
         break;
     }
-  }, []);
+  }, [idUser, token, role, onlyAdmin, onlyManagerOrAdmin, navigate, resetAllStates]);
 
-  // if (!idUser) return <FullPage />;
+  if (!idUser || !token) return <FullPage />; // ← DESCOMENTAR
 
   if (isChecking) {
     return <FullPage />;
